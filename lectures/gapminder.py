5c3623d47ebbcd15ee8116861b26730814514f2d
Ali Leylani <ali.leylani@quantumstate.se>
2023-11-02 15:04:32 +0100

    Update gapminder.py

--- lectures/gapminder.py
+++ lectures/gapminder.py
@@ -4,7 +4,7 @@ gapminder = px.data.gapminder()
 fig = px.scatter(gapminder, x="gdpPercap", y="lifeExp",
            size="pop", color="country", size_max=70, log_x=True,
            animation_frame="year", animation_group="country", title="Gapminder",
-           range_x=[100, 100000], range_y=[25,90])
+           range_x=[100, 100000], range_y=[0,90])
 fig.show()
 
 #fig.write_html('gapminder.html', auto_open=True)
